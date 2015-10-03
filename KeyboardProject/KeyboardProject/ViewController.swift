7e80b35d74de153419aec0dc7f6dda66165f2301
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-10-03 19:59:20 +1000

    Add 'KeyboardProject' Xcode project

---
+++ KeyboardProject/KeyboardProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyboardProject
+//
+//  Created by Yichi on 3/10/2015.
+//  Copyright © 2015 Yichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
