43469f615cf6f5d02e222e097ced0dd7a1cbc0af
Yichi Zhang <zhang-yi-chi@hotmail.com>
2016-03-11 01:48:00 +1100

    Format source code

--- KeyboardProject/CustomHeightKeyboard/KeyboardViewController.swift
+++ KeyboardProject/CustomHeightKeyboard/KeyboardViewController.swift
@@ -8,84 +8,91 @@
 
 import UIKit
 
-class KeyboardViewController: UIInputViewController {
-
+class KeyboardViewController: UIInputViewController
+{
     @IBOutlet var nextKeyboardButton: UIButton!
-    var heightConstraint:NSLayoutConstraint!
-    
-    override func updateViewConstraints() {
+    var heightConstraint: NSLayoutConstraint!
+
+    override func updateViewConstraints()
+    {
         super.updateViewConstraints()
-    
+
         // Add custom view sizing constraints here
         if (view.frame.size.width == 0 || view.frame.size.height == 0) {
             return
         }
-        
+
         setUpHeightConstraint()
     }
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-    
+
         // Perform custom UI setup here
         self.nextKeyboardButton = UIButton(type: .System)
-    
+
         self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
         self.nextKeyboardButton.sizeToFit()
         self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-    
+
         self.nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
-        
+
         self.view.addSubview(self.nextKeyboardButton)
     }
-    
-    override func viewDidAppear(animated: Bool) {
+
+    override func viewDidAppear(animated: Bool)
+    {
         super.viewDidAppear(animated)
-        
+
         let nextKeyboardButtonLeftSideConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1.0, constant: 0.0)
         let nextKeyboardButtonBottomConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
         self.view.addConstraints([nextKeyboardButtonLeftSideConstraint, nextKeyboardButtonBottomConstraint])
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated
     }
 
-    override func textWillChange(textInput: UITextInput?) {
+    override func textWillChange(textInput: UITextInput?)
+    {
         // The app is about to change the document's contents. Perform any preparation here.
     }
 
-    override func textDidChange(textInput: UITextInput?) {
+    override func textDidChange(textInput: UITextInput?)
+    {
         // The app has just changed the document's contents, the document context has been updated.
-    
+
         var textColor: UIColor
         let proxy = self.textDocumentProxy
         if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
             textColor = UIColor.whiteColor()
-        } else {
+        }
+        else {
             textColor = UIColor.blackColor()
         }
         self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
     }
 
-    func setUpHeightConstraint() {
+    func setUpHeightConstraint()
+    {
         let customHeight = UIScreen.mainScreen().bounds.height / 2
-        
+
         if heightConstraint == nil {
-            heightConstraint = NSLayoutConstraint(
-                item: view,
-                attribute: .Height,
-                relatedBy: .Equal,
-                toItem: nil,
-                attribute: .NotAnAttribute,
-                multiplier: 1,
-                constant: customHeight
-            )
+            heightConstraint = NSLayoutConstraint(item: view,
+                                                  attribute: .Height,
+                                                  relatedBy: .Equal,
+                                                  toItem: nil,
+                                                  attribute: .NotAnAttribute,
+                                                  multiplier: 1,
+                                                  constant: customHeight)
             heightConstraint.priority = UILayoutPriority(999)
-            
+
             view.addConstraint(heightConstraint)
-        } else {
+        }
+        else {
             heightConstraint.constant = customHeight
         }
     }
